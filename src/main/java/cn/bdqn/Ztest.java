3053e103f34e64607a813c19f5bcf7b35685bb84
xuesong0114 <904102304@qq.com>
2017-08-03 18:52:56 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/Ztest.java
@@ -0,0 +1,13 @@
+package cn.bdqn;
+
+import org.junit.Test;
+
+/**
+ * Created by feng0114 on 2017/8/3.
+ */
+public class Ztest {
+    @Test
+    public void text(){
+        System.out.println("我了个去");
+    }
+}
